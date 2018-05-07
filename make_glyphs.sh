1797b7e0a5e1802cd43a0c6eeda22fcf94c2bd77
Alexey Abdusamatov <ajiekc905@gmail.com>
2018-05-07 13:01:54 +1000

    fixed width calculation, started to add 1bit bitmap saver

--- make_glyphs.sh
+++ make_glyphs.sh
@@ -1,5 +1,5 @@
 # fontsize=18
-fontsize=22
+fontsize=20
 maxWidth=16
 maxHeight=16
 outFormat=bmp
@@ -22,40 +22,61 @@ function symbolsRange {
   done
 }
 
+function write1bitbmp {
+  header='\x42\x4D\x7E\x00\x00\x00\x00\x00\x00\x00\x3E\x00\x00\x00\x28\x00\x00\x00\x10\x00\x00\x00\x10\x00\x00\x00\x01\x00\x01\x00\x00\x00\x00\x00\x40\x00\x00\x00\xC3\x0E\x00\x00\xC3\x0E\x00\x00\x02\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\xFF\xFF\xFF\xFF'
+
+  # convert magick:wizard -monochrome -colors 2 -crop 16x16  $1
+
+  printf $header > $2
+  cat $1 >> $2
+}
+
 function process {
     # echo $1 $2
     currentSymbol=$1
     symbolCode=$2
-    sizes=$(convert -background black -fill white -pointsize $fontsize -gravity center +antialias -font $font label:"$currentSymbol" -trim  info:- 2>/dev/null)
+    sizes=$(convert -background black -fill white -pointsize $fontsize -gravity West +antialias -font $font label:"$currentSymbol" -trim  info:- 2>/dev/null)
     trimmedSize=$(echo $sizes|awk '{ print $3 }')
     symbolWidth=$(echo $trimmedSize| awk -F'x' '{ print $1 }')
     # Width=$(printf "%01X" "$symbolWidth")
-    Width2=$(printf "%01X" $(($symbolWidth+1)))
+    Width2=$(printf "%01X" $(($symbolWidth-1)))
     # echo original $Width plus one $Width2
 
     symbolHeight=$(echo $trimmedSize| awk -F'x' '{ print $2 }')
     canvas=$(echo $sizes|awk '{ print $4 }')
     cXShift=$(echo $canvas| awk -F'[+x]' '{ print $3 }')
     cYShift=$(echo $canvas| awk -F'[+x]' '{ print $4 }')
+    # top=$(($cYShift-2))
+    top=0
 
 
     if [[ "$trimmedSize" = '1x1' ]]; then
       echo $symbolCode 'no glyph'
     else
       echo $symbolCode "( $symbolWidth x $symbolHeight )"  shifted $cXShift right and $cYShift down $currentSymbol
+      # echo $symbolCode "($symbolWidth x $symbolHeight)" $currentSymbol
       if (( $cYShift > 4 )); then
         cYShift=4
       fi
       # crop $cYShift from top, $cXShift from left, $symbolWidth, $symbolHeight<=16 and save   (-crop +top+left +repage)
-      convert -background black -fill white -pointsize $fontsize -gravity center +antialias -font $font label:"$currentSymbol"  -crop 16x16+$cYShift+$cXShift +repage -set page 16x16+0+0 -flatten   BMP/"$hex"_"$Width2""$cYShift".$outFormat
+      # convert -background black -fill white -pointsize $fontsize -gravity West +antialias -font $font label:"$currentSymbol"  -crop 16x16+$cXShift+$top  -set page 16x16+0+0 -flatten -colors 2 +dither -type bilevel  BMP/"$hex"_"$Width2""$cYShift".$outFormat
+      # convert -background black -fill white -pointsize $fontsize -gravity West +antialias -font $font label:"$currentSymbol"  -crop 16x16+$cXShift+$top  -set page 16x16+0+0 -flatten -colors 2 +dither -type bilevel  MONO:BMP/tempbindata.bin
+      convert -background black -fill white -pointsize $fontsize -gravity West +antialias -font $font label:"$currentSymbol"  -crop 16x16+$cXShift+$top -set page 16x16+0+0 -flatten -colors 2 +dither -type bilevel  MONO:BMP/"$hex"_"$Width2""$cYShift".bin
+      convert -background black -fill white -pointsize $fontsize -gravity West +antialias -font $font label:"$currentSymbol"  -crop 16x16+$cXShift+$top -set page 16x16+0+0 -flatten -colors 2 +dither -type bilevel  PNG:BMP/"$hex"_"$Width2""$cYShift".png
+      # write1bitbmp BMP/tempbindata.bin BMP/"$hex"_"$Width2""$cYShift".$outFormat
     fi
 }
 
-symbolsRange "0x0591" "0x05C7"
-symbolsRange "0x05D0" "0x05EA"
-symbolsRange "0xFB1D" "0xFB36"
+
+############ Herbew start
+# symbolsRange "0x0591" "0x05C7"
+# symbolsRange "0x05D0" "0x05EA"
+# symbolsRange "0xFB1D" "0xFB36"
 symbolsRange "0xFB38" "0xFB3C"
-symbolsRange "0xFB3E" "0xFB3E"
-symbolsRange "0xFB40" "0xFB41"
-symbolsRange "0xFB43" "0xFB44"
-symbolsRange "0xFB46" "0xFB4F"
+# symbolsRange "0xFB3E" "0xFB3E"
+# symbolsRange "0xFB40" "0xFB41"
+# symbolsRange "0xFB43" "0xFB44"
+# symbolsRange "0xFB46" "0xFB4F"
+############ end
+
+symbolsRange "0x30" "0x3F"
