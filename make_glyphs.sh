cc11cd84613d30112b36adc8a628e67cbb3b5c02
Alexey Abdusamatov <ajiekc905@gmail.com>
2018-05-08 14:17:39 +1000

    Feature flexible size and font per symbol range

--- make_glyphs.sh
+++ make_glyphs.sh
@@ -10,7 +10,7 @@ font=ArialHebrew-Bold-02.ttf
 
 
 
-commonParams="-background black -fill white -gravity NorthWest +antialias -pointsize $fontsize -font $font"
+commonParams="-background black -fill white -gravity NorthWest +antialias"
 borderParams="-bordercolor black -border 1x1"
 rm -rf ./BMP/
 mkdir ./BMP
@@ -20,11 +20,17 @@ echo '{"header":[78,69,90,75,8,255,255,255,255,255,1,0,0,0,255,255,255,255,255,2
 function symbolsRange {
   current=$(printf '%d' $1)
   stop=$(printf '%d' $2)
+  param3=$3
+  param4=$4
+  ### https://stackoverflow.com/questions/3953645/ternary-operator-in-bash#3953712
+  fontLocalSize=${param3:-$fontsize}
+  fontLocalName=${param4:-$font}
+  echo using size $fontLocalSize and font $fontLocalName
   while test "$current" -le "$stop"; do
     hex=$(printf "%04X" "$current")
     symbol=$(printf "\u"$hex)
     echo "$symbol" $hex
-    process "$symbol" $hex
+    process "$symbol" $hex $fontLocalSize $fontLocalName
     current=$((current+1))
   done
 }
@@ -33,8 +39,12 @@ function symbolsRange {
 function process {
     currentSymbol="$1"
     symbolCode=$2
+    fSize=$3
+    fName=$4
+    fParams="-pointsize $fSize -font $fName"
+
     label="label:"\\"$currentSymbol"
-    sizes=$(convert $commonParams $label $borderParams -trim  info:- 2>/dev/null)
+    sizes=$(convert $commonParams $fParams $label $borderParams -trim  info:- 2>/dev/null)
     trimmedSize=$(echo $sizes|awk '{ print $3 }')
     symbolWidth=$(echo $trimmedSize| awk -F'x' '{ print $1 }')
     Width2=$(printf "%01X" $(($symbolWidth-1)))
@@ -69,7 +79,7 @@ function process {
       secondTransform="convert -size 16x16 MONO:-"
       generatedFilename="$hex"_"$Width2""$topHex"
 
-      convert $commonParams $label $cropPage \
+      convert $commonParams $fParams $label $cropPage \
         $firstTransform|$secondTransform BMP3:BMP/$generatedFilename.bmp
 
     fi
@@ -77,21 +87,26 @@ function process {
 
 
 ############ Hebrew start
-symbolsRange "0x0591" "0x05C7"
-symbolsRange "0x05D0" "0x05EA"
-symbolsRange "0xFB1D" "0xFB36"
-symbolsRange "0xFB38" "0xFB3C"
-symbolsRange "0xFB3E" "0xFB3E"
-symbolsRange "0xFB40" "0xFB41"
-symbolsRange "0xFB43" "0xFB44"
-symbolsRange "0xFB46" "0xFB4F"
+# symbolsRange "0x0591" "0x05C7"
+# symbolsRange "0x05D0" "0x05EA"
+# symbolsRange "0xFB1D" "0xFB36"
+# symbolsRange "0xFB38" "0xFB3C"
+# symbolsRange "0xFB3E" "0xFB3E"
+# symbolsRange "0xFB40" "0xFB41"
+# symbolsRange "0xFB43" "0xFB44"
+# symbolsRange "0xFB46" "0xFB4F"
 ############ end
 
 ############ askii
-symbolsRange "0x29" "0x2B"
-symbolsRange "0x5B" "0x5D"
-symbolsRange "0x30" "0x39"
+# symbolsRange "0x29" "0x2B"
+# symbolsRange "0x5B" "0x5D"
+symbolsRange "0x30" "0x31"
+symbolsRange "0x32" "0x33" 21
+symbolsRange "0x34" "0x35" 22
+symbolsRange "0x36" "0x37" 23
+symbolsRange "0x38" "0x39" 24
+
 # 5c "\" causing error ??? escaping problem
 # 2a - the same
-symbolsRange "0x2B" "0x5B"
-symbolsRange "0x5D" "0x5B"
+# symbolsRange "0x2B" "0x5B" 22
+# symbolsRange "0x5D" "0x5B" 23
