89ceb47d760ba6ded3622685dfd874d995b2551d
Alexey <ajiekc905@gmail.com>
2018-05-16 14:45:33 +1000

    fixed for bash 3.x

--- make_glyphs.sh
+++ make_glyphs.sh
@@ -15,127 +15,160 @@ rm -rf ./BMP/
 mkdir ./BMP
 echo '{"header":[78,69,90,75,8,255,255,255,255,255,1,0,0,0,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255],"version":8}' > ./BMP/font_info.json
 
+function codepoint2Utf {
+  local utfDecimal=$1
+  local result=''
+  local bytes=0
+  while test "$utfDecimal" -gt 0; do
+    result="$(printf '\\x%X' $(($utfDecimal & 63 |128)))$result"
+    bytes=$(($bytes+1))
+    utfDecimal=$(($utfDecimal>>6 )) 
+    if [[ $utfDecimal -lt 128 ]]; then
+      if [[ $bytes -eq 2 ]]; then
+        result="$(printf '\\x%X' $(($utfDecimal |224)))$result"
+      fi
+      if [[ $bytes -eq 1 ]]; then
+        result="$(printf '\\x%X' $(($utfDecimal |192)))$result"
+      fi
+      utfDecimal=$(($utfDecimal>>6 )) 
+    fi
+  done
+  echo $result
+}
 
 function symbolsRange {
-  current=$(printf '%d' $1)
-  stop=$(printf '%d' $2)
-  param3=$3
-  param4=$4
-  ### https://stackoverflow.com/questions/3953645/ternary-operator-in-bash#3953712
-  fontLocalSize=${param3:-$fontsize}
-  fontLocalName=${param4:-$font}
-  echo using size $fontLocalSize and font $fontLocalName
-  local YCORR=$(getVertCorrection 0 0030 $fontLocalSize $fontLocalName)
-  echo standart shift is $YCORR
-  while test "$current" -le "$stop"; do
-    hexPadded=$(printf "%04X" "$current")
-    hex=$(printf "%X" "$current")
-    symbol=$(printf "\x$hex")
-    echo "$symbol" $hexPadded $current
-    process "$symbol" $hexPadded $fontLocalSize $fontLocalName
-    current=$((current+1))
-  done
+    current=$(printf '%d' $1)
+    stop=$(printf '%d' $2)
+    param3=$3
+    param4=$4
+    ### https://stackoverflow.com/questions/3953645/ternary-operator-in-bash#3953712
+    fontLocalSize=${param3:-$fontsize}
+    fontLocalName=${param4:-$font}
+    echo using size $fontLocalSize and font $fontLocalName
+    local YCORR=$(getVertCorrection 0 0030 $fontLocalSize $fontLocalName)
+    echo standart shift is $YCORR
+    while test "$current" -le "$stop"; do
+        hexPadded=$(printf "%04X" "$current")
+        hex=$(printf "%X" "$current")
+        # symbol=$(printf "\x$hex")
+        symbol=$(printf $(codepoint2Utf $current))
+
+        # echo -ne '\xd0\xa4' bash 3x way to print utf symbols
+        echo "$symbol" $hexPadded
+        process "$symbol" $hexPadded $YCORR $fontLocalSize $fontLocalName
+        current=$((current+1))
+    done
 }
 
 function codeFromSymbol {
-  printf "%x\n" \'A
+    printf "%x\n" \'A
 }
 
 function symbolsInString {
-  charsList=$1
-  param3=$2
-  param4=$3
-  fontLocalSize=${param3:-$fontsize}
-  fontLocalName=${param4:-$font}
-  echo using size $fontLocalSize and font $fontLocalName
-  local YCORR=$(getVertCorrection 0 0030 $fontLocalSize $fontLocalName)
-  echo standart shift is $YCORR
-  for (( i=0; i<${#charsList}; i++ )); do
-  symbol=${charsList:$i:1}
-  hex=$(echo -ne $symbol | iconv -t utf-16be|od -t x1 -A none|sed  's/ //g')
-  # hex=$(printf "%04X" \'$symbol)
-  # don't work under bash 3.2 high sierra default
-    echo "$symbol" $hex
-    process "$symbol" $hex $fontLocalSize $fontLocalName
-  done
+    charsList=$1
+    param3=$2
+    param4=$3
+    fontLocalSize=${param3:-$fontsize}
+    fontLocalName=${param4:-$font}
+    echo using size $fontLocalSize and font $fontLocalName
+    local YCORR=$(getVertCorrection 0 0030 $fontLocalSize $fontLocalName)
+    echo standart shift is $YCORR
+    for (( i=0; i<${#charsList}; i++ )); do
+        symbol=${charsList:$i:1}
+        hex=$(echo -ne $symbol | iconv -t utf-16be|od -t x1 -A none|sed  's/ //g')
+        # hex=$(printf "%04X" \'$symbol)
+        # don't work under bash 3.2 high sierra default
+        echo "$symbol" $hex
+        process "$symbol" $hex $YCORR $fontLocalSize $fontLocalName
+    done
 }
 
 function getVertCorrection {
-  # the main idea is to call this function with symbol 0 or any standart capital letter (no diacritics) 
-  # to extract standart vertical shift. It will be used as 4 pix from top and others shifts will be relative 
-  # this one as it used in original fonts
+    # the main idea is to call this function with symbol 0 or any standart capital letter (no diacritics)
+    # to extract standart vertical shift. It will be used as 4 pix from top and others shifts will be relative
+    # this one as it used in original fonts
     currentSymbol="$1"
     symbolCode=$2
     fSize=$3
     fName=$4
     fParams="-pointsize $fSize -font $fName"
-
+    
     label="label:"\\"$currentSymbol"
     sizes=$(convert $commonParams $fParams $label $borderParams -trim  info:- 2>/dev/null)
     canvas=$(echo $sizes|awk '{ print $4 }')
     local YShift=$(echo $canvas| awk -F'[+x]' '{ print $4 }')
     echo $(($YShift-4))
-
+    
 }
 
 function process {
     currentSymbol="$1"
     symbolCode=$2
-    fSize=$3
-    fName=$4
+    verticalCorrector=$3
+    fSize=$4
+    fName=$5
     fParams="-pointsize $fSize -font $fName"
-
+    
     label="label:"\\"$currentSymbol"
     sizes=$(convert $commonParams $fParams $label $borderParams -trim  info:- 2>/dev/null)
     trimmedSize=$(echo $sizes|awk '{ print $3 }')
     symbolWidth=$(echo $trimmedSize| awk -F'x' '{ print $1 }')
-    Width2=$(printf "%01X" $(($symbolWidth-1)))
-
+    # it could be wider than 16, so we should limit it to 16
+    
     symbolHeight=$(echo $trimmedSize| awk -F'x' '{ print $2 }')
     canvas=$(echo $sizes|awk '{ print $4 }')
     cXShift=$(echo $canvas| awk -F'[+x]' '{ print $3 }')
     cYShift=$(echo $canvas| awk -F'[+x]' '{ print $4 }')
-
-
+    
+    
     if [[ "$trimmedSize" = '1x1' ]]; then
-      echo $symbolCode 'no glyph'
+        echo $symbolCode 'no glyph'
     else
-      # if (( $cYShift > 4 )); then
-      #   cYShift=4
-      # fi
-    # top=$cYShift
-    top=$(($cYShift-0))
-    topHex=$(printf "%01X" $top)
-    # TODO find out a magick correction number to decrease topHex parameter (last digit in filename)
-
-      # IM can not produce BMP's at depth levels other than 8.  However you can
-      #   use NetPBM image processing set to do the final conversion to other depth
-      #   levels (This needs at least a Q16 version of IM)...
-      #       convert image +matte -colors 16 ppm:- |\
-      #         pnmdepth 4 | ppm2bmp > image.bmp
-      # https://www.imagemagick.org/Usage/formats/#bmp
-
-      cropPage="$borderParams -crop 16x16+$cXShift+$top -set page 16x16+0+0"
-      firstTransform="-flatten -colors 4 +dither -type bilevel  MONO:"
-      secondTransform="convert -size 16x16 MONO:-"
-      generatedFilename="$hex"_"$Width2""$topHex"
-
-      convert $commonParams $fParams $label $cropPage \
+        top=$cYShift
+        topCorrected=$(($cYShift-$verticalCorrector))
+        # && (( $symbolHeight < 16 ))
+        echo $currentSymbol $canvas $verticalCorrector $cYShift $topCorrected
+        # if (( $topCorrected > 4 )) ; then
+        #  topCorrected=4
+        top=$(($verticalCorrector+4))
+        # fi
+        if (( $symbolWidth >16)); then
+            symbolWidth=16
+        fi
+        Width2=$(printf "%01X" $(($symbolWidth-1)))
+        
+        topHex=$(printf "%01X" $topCorrected)
+        # TODO find out a magick correction number to decrease topHex parameter (last digit in filename)
+        
+        # IM can not produce BMP's at depth levels other than 8.  However you can
+        #   use NetPBM image processing set to do the final conversion to other depth
+        #   levels (This needs at least a Q16 version of IM)...
+        #       convert image +matte -colors 16 ppm:- |\
+        #         pnmdepth 4 | ppm2bmp > image.bmp
+        # https://www.imagemagick.org/Usage/formats/#bmp
+        
+        cropPage="$borderParams -crop 16x16+$cXShift+$top -set page 16x16+0+0"
+        # cropPage="$borderParams -crop 16x16+$cXShift+$topCorrected -set page 16x16+0+0"
+        firstTransform="-flatten -colors 4 +dither -type bilevel  MONO:"
+        secondTransform="convert -size 16x16 MONO:-"
+        generatedFilename="$hex"_"$Width2""$topHex"
+        
+        convert $commonParams $fParams $label $cropPage \
         $firstTransform|$secondTransform BMP3:BMP/$generatedFilename.bmp
-
+        
     fi
 }
 
 
 ############ Hebrew start
 # symbolsRange "0x0591" "0x05C7"
-# symbolsRange "0x05D0" "0x05EA"
+symbolsRange "0x05D0" "0x05EA"
 # symbolsRange "0xFB1D" "0xFB36"
 # symbolsRange "0xFB38" "0xFB3C"
 # symbolsRange "0xFB3E" "0xFB3E"
 # symbolsRange "0xFB40" "0xFB41"
 # symbolsRange "0xFB43" "0xFB44"
-# symbolsRange "0xFB46" "0xFB4F"
+symbolsRange "0xFB46" "0xFB4F"
 ############ end
 
 # symbolsRange "0x21" "0x2F" 18 Arial_Black.ttf
@@ -174,3 +207,4 @@ function process {
 # symbolsRange "0x450" "0x45F" 18 Arial_Black.ttf
 
 symbolsInString "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЭЮЯ" 19 Arial_Black.ttf
+symbolsInString "jklmoz0yfb" 19 Arial_Black.ttf
